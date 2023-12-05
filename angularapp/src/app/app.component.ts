e6e456b277e3debdf54b5e9f4367aecbe3b350a3
Iamneo <techadmin@iamneo.ai>
2023-12-05 07:21:50 +0000

    Manual-save-commit-05/12/2023--12:51:48

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -8,3 +8,14 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angularapp';
 }
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angularapp';
+}
